a0ae5077c41e006e47507ce4937b0fef61341780
Stephen-T-Homick <shomick87@gmail.com>
2018-03-11 01:17:40 -0500

    Added comments / logging

--- zvolmount.sh
+++ zvolmount.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-# Mounts zvols on boot
+# This script will mount specified zvols on boot.
+# Set this in crontab as '@reboot' notation to start on boot.
 
-set -eu
+set -o pipefail   # If a command anywhere in the set of piped commands failed, the whole line should fail
+set -u  # Uninitialized variables should cause an error
+set -e # Exit the script if any command fails
 
+# Adjust zvol1 and zvol2 / pool as needed for your own storage pool(s) / zvol mounting naming conventions.
+
+iso="/dev/zvol/loftPool/home/iso-part1" #zvol1
+poolimport=`zpool import | wc -l`
 pool="loftPool"
-loftpoolimport=`zpool import | wc -l` 
-shomenas="/dev/zvol/loftPool/home/shomeNAS-part1"
-iso="/dev/zvol/loftPool/home/iso-part1"
+shomenas="/dev/zvol/loftPool/home/shomeNAS-part1" #zvol2
 
-if [ $loftpoolimport -gt 0 ] ; then # If the wc of zpool import is greater than 1, pool is likely not imported.
-	logger "$pool not imported. Check to make sure drives are inserted and canmount property is set to 'on' "
+if [ $poolimport -gt 0 ] ; then # If the wc value of zpool import is greater than 1, pool is likely not imported.
+	logger "$pool not imported. Check to make sure drives are inserted and canmount property is set to 'on' " # Appropriate syslog'ging
 	$(command -v zpool) import $pool #Imports pool
 else
 	logger "loftPool already imported. Mounting zvols"
@@ -19,10 +24,9 @@ fi # if [ $loftpoolimport -gt 0 ] ; then
 
 if [ -b $shomenas ] && [ -b $iso ]; then # If both block devices exist.
 	(mount $shomenas /mnt/shomeNAS ; mount $iso /mnt/iso) && logger "shomeNAS and iso zvols mounted successfully" || logger "trouble mounting zvols" # Mounts them both successfully
-	if [ $? -ne 0 ] ; then # If exit status is anything less than successful (0) 
-		logger "assetremount - trouble mounting zvols. Check to make sure pool(s) are imported."
+	if [ $? -ne 0 ] ; then # If exit status is anything less than successful (0)
+		logger "$0 assetremount - trouble mounting zvols. Check to make sure pool(s) are imported."
 	fi #if [ $? -ne 0 ]
 else
-	logger "Trouble mounting zvols. Check to make sure pool(s) are imported, and drive(s) are properly inserted."
+	logger "$0 Trouble mounting zvols. Check to make sure pool(s) are imported, and drive(s) are properly inserted."
 fi #if [ -b $shomenas ] && [ -b $iso ]; then
-
